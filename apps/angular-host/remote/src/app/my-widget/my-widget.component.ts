6debea235fe14c288a0a5bccf96c9e20e0c9eaf4
กฤต <112327677+biskitsx@users.noreply.github.com>
2025-05-23 13:50:02 +0700

    feat: update shell

---
+++ apps/angular-host/remote/src/app/my-widget/my-widget.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-my-widget',
+  imports: [CommonModule],
+  templateUrl: './my-widget.component.html',
+  styleUrl: './my-widget.component.css',
+})
+export class MyWidgetComponent {}
